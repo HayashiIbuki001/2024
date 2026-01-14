65bb79124019478abb01d0d74ac76b90668379eb
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2026-01-14 14:17:05 +0900

    [追加]重力計算

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -9,6 +9,18 @@ public class BoardManager : MonoBehaviour
 
     void Start()
     {
+        SetBoard();
+        DropNumber(1, 2);
+        DropNumber(1, 4);
+        ApplyGravity();
+        PrintBoard();
+    }
+
+    /// <summary>
+    /// 初期配置のため0を全マスに入れる
+    /// </summary>
+    private void SetBoard()
+    {
         board = new int[width, height];
 
         for (int y = 0; y < height; y++)
@@ -18,13 +30,13 @@ public class BoardManager : MonoBehaviour
                 board[x, y] = 0;
             }
         }
-
-        DropNumber(1, 2);
-        DropNumber(1, 4);
-        ApplyGravity();
-        PrintBoard();
     }
 
+    /// <summary>
+    /// 数字を落とすメソッド
+    /// </summary>
+    /// <param name="x">マスの行</param>
+    /// <param name="value">入れる数字</param>
     void DropNumber(int x, int value)
     {
         for (int y = 0; y < 4; y++)
@@ -39,6 +51,9 @@ public class BoardManager : MonoBehaviour
         Debug.Log("ここには設置できません。");
     }
 
+    /// <summary>
+    /// 現在のボードをLogに出力
+    /// </summary>
     void PrintBoard()
     {
         for (int y = height - 1; y >= 0; y--)
@@ -52,16 +67,28 @@ public class BoardManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 重力を計算する
+    /// </summary>
     void ApplyGravity()
     {
-        for (int x = 0; x < width; x++)
+        bool moved;
+
+        do
         {
-            for (int y = 1; y < height; y++)
-                if (board[x, y] != 0 && board[x, y - 1] == 0)
-                {
-                    board[x, y - 1] = board[x, y];
-                    board[x, y] = 0;
-                }
-        }
+            moved = false;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 1; y < height; y++)
+                    if (board[x, y] != 0 && board[x, y - 1] == 0)
+                    {
+                        board[x, y - 1] = board[x, y];
+                        board[x, y] = 0;
+
+                        moved = true;
+                    }
+            }
+        } while (moved);
     }
 }
