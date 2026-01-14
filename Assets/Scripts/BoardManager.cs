63179d22ba0b40b4dd0c5c91d7c0db11f507ad64
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2026-01-14 20:17:57 +0900

    [追加]縦横の合体処理完成

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
+    [SerializeField] private GameObject cellPrefab;
+    [SerializeField] private float cellSize = 1.1f;
+
+    private CellView[,] cells;
+
     [SerializeField] private int width = 4; // 横
     [SerializeField] private int height = 4; // 縦
 
@@ -12,10 +17,12 @@ public class BoardManager : MonoBehaviour
     void Start()
     {
         SetBoard();
+        CreateGrid();
 
         // 盤面下段を作る
         DropNumber(0, 2); // 左
         DropNumber(2, 2); // 右
+        ClearIsNew();
 
         // 真ん中に主役を落とす
         DropNumber(1, 2);
@@ -23,6 +30,7 @@ public class BoardManager : MonoBehaviour
         ApplyMerge();
         ClearIsNew();
         PrintBoard();
+        UpdateView();
     }
 
     /// <summary>
@@ -39,6 +47,30 @@ public class BoardManager : MonoBehaviour
             {
                 board[x, y] = 0;
                 isNew[x, y] = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 盤面を生成する
+    /// </summary>
+    private void CreateGrid()
+    {
+        cells = new CellView[width, height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                GameObject obj = Instantiate(cellPrefab);
+                obj.transform.position = new Vector3(x * cellSize, y * cellSize, 0);
+
+
+                CellView cell = obj.GetComponent<CellView>();
+                cell.x = x;
+                cell.y = y;
+
+                cells[x,y] = cell;
             }
         }
     }
@@ -103,9 +135,14 @@ public class BoardManager : MonoBehaviour
                         moved = true;
                     }
             }
-        } while (moved);
+
+        } while (moved); // 処理が止まるまで動かす
+        UpdateView();
     }
 
+    /// <summary>
+    /// 縦横の合体処理
+    /// </summary>
     void ApplyMerge()
     {
         bool merged;
@@ -115,13 +152,20 @@ public class BoardManager : MonoBehaviour
             merged = false;
 
             if (ApplyMergeVertical())
+            {
                 merged = true;
+                continue;
+            }
 
             if (ApplyMergeHorizontal())
                 merged = true;
-        } while (merged);
+        } while (merged); // 処理が止まるまで回す
     }
 
+    /// <summary>
+    /// 縦行の合体処理(最優先)
+    /// </summary>
+    /// <returns>合体したかどうかのフラグ</returns>
     bool ApplyMergeVertical()
     {
         bool merged = false;
@@ -130,10 +174,11 @@ public class BoardManager : MonoBehaviour
         {
             for (int y = 1; y < height; y++)
             {
+                if (!isNew[x, y]) continue;
                 if (board[x, y] != 0 && board[x, y] == board[x, y - 1])
                 {
-                    board[x, y - 1] *= 2;
-                    board[x, y] = 0;
+                    board[x, y - 1] *= 2; // 最新で更新されたマスを倍に
+                    board[x, y] = 0; // 合体されたマスを0に
 
                     isNew[x, y - 1] = true;
                     merged = true;
@@ -144,6 +189,10 @@ public class BoardManager : MonoBehaviour
         return merged;
     }
 
+    /// <summary>
+    /// 横列の合体処理
+    /// </summary>
+    /// <returns>合体したかどうかのフラグ</returns>
     bool ApplyMergeHorizontal()
     {
         bool merged = false;
@@ -152,36 +201,52 @@ public class BoardManager : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
+                if (!isNew[x, y]) continue;
                 if (board[x, y] != 0 && x > 0 && board[x, y] == board[x - 1, y])
                 {
-                    board[x, y] *= 2;
-                    board[x - 1, y] = 0;
+                    board[x, y] *= 2; // 最新で更新されたマスを倍に
+                    board[x - 1, y] = 0; // 合体されたマスを0に
 
                     isNew[x, y] = true;
                     isNew[x - 1, y] = false;
                     merged = true;
                     ApplyGravity();
+                    return merged;
                 }
 
                 if (isNew[x, y] && x < width - 1 && board[x, y] == board[x + 1, y])
                 {
-                    board[x, y] *= 2;
-                    board[x + 1, y] = 0;
+                    board[x, y] *= 2; // 最新で更新されたマスを倍に
+                    board[x + 1, y] = 0; // 合体されたマスを0に
 
                     isNew[x,y] = true;
                     isNew[x + 1, y] = false;
                     merged = true;
                     ApplyGravity();
+                    return merged;
                 }
             }
         }
         return merged;
     }
 
+    /// <summary>
+    /// 全てのマスのisNewをfalseにリセット
+    /// </summary>
     void ClearIsNew()
     {
         for (int y = 0; y < height; y++)
             for (int x = 0; x < width; x++)
                 isNew[x, y] = false;
+    }
+
+    /// <summary>
+    /// UI更新
+    /// </summary>
+    private void UpdateView()
+    {
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+                cells[x, y].SetValue(board[x, y]);
     }
 }
--- Assets/Scripts/CellView.cs
+++ Assets/Scripts/CellView.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class CellView : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int x;
+    public int y;
+
+    [SerializeField] private TextMeshPro valueText;
+    [SerializeField] private SpriteRenderer bg;
+
+    private void Start()
     {
-        
+        bg = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetValue(int value)
     {
-        
+        valueText.text = value == 0 ? "" : value.ToString();
+        bg.color = GetColor(value);
     }
+
+    Color GetColor(int value)
+    {
+        if (value == 0) return Color.gray;
+        if (value == 2) return new Color(0.9f, 0.9f, 0.8f);
+        if (value == 4) return new Color(0.9f, 0.8f, 0.6f);
+        return Color.white;
+    }
+
+
 }
+
