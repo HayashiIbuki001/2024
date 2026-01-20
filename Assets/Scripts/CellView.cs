c8cf78eed11dd77136f8c882b7ba821ba1987443
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2026-01-20 17:51:27 +0900

    [変更]セルに色付けなおした

--- Assets/Scripts/CellView.cs
+++ Assets/Scripts/CellView.cs
@@ -6,12 +6,24 @@ public class CellView : MonoBehaviour
 {
     [SerializeField] TextMeshPro valueText;
     [SerializeField] SpriteRenderer bg;
+    [SerializeField] private int maxLevel = 11;
 
     Tween moveTween;
 
     public void SetValue(int v)
     {
         valueText.text = v == 0 ? "" : v.ToString();
+
+        // 色
+        int level = (int)Mathf.Log(v, 2);
+        float t = Mathf.Clamp01(level / (float)maxLevel);
+
+        float hue = Mathf.Lerp(0.6f, 0.0f, t);
+        float sat = 0.8f;
+        float val = 0.9f;
+
+        Color c = Color.HSVToRGB(hue, sat, val);
+        bg.color = c;
     }
 
     public void MoveTo(Vector3 pos, float duration)
